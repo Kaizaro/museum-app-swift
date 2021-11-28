cdac369601565230122abb0af74194589fd2decd
Kaizaro <kaizaro78@gmail.com>
2021-11-28 15:18:46 +0300

    Initial Commit

---
+++ MuseumAppSwift/MuseumAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumAppSwiftApp.swift
+//  MuseumAppSwift
+//
+//  Created by Paul Zamshin on 28.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MuseumAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
